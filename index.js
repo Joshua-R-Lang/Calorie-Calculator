9e4e87d34a188d8199140b739296b2ee79680b6e
Joshua Lang <joshuarlangg@gmail.com>
2024-06-28 17:59:34 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let sex;
+let height;
+let weight;
+let activityLevel;
+
+document.getElementById("Calculate_Button").onclick = function(){
+    height = document.getElementById("Height_Textbox").value;
+    weight = document.getElementById("Weight_Textbox").value;
+    console.log(height);
+    console.log(weight);
+}
